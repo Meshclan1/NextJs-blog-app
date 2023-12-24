2474c8c1c529e1a619ccf852c5732c1162f5b6a4
Ayo <Ayo.meshe@gmail.com>
2023-12-24 19:08:45 +0000

    designing post page

---
+++ app/home/PostCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import type { Post } from "@prisma/client";
+
+type Props = {
+  post: Post;
+  className?: string;
+};
+
+const PostCard = ({ post, className }: Props) => {
+  return (
+    <div
+      className={`${className} p-4 rounded border-2 shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)]`}
+    >
+      <h3 className="text-3xl mb-3">{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+};
+
+export default PostCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./home/HeroSection";
+import Posts from "./home/Posts";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Posts />
     </>
   );
 }
