b738baeea58c05760e30774383c085331263a3e8
Ayo <Ayo.meshe@gmail.com>
2023-12-27 01:35:12 +0000

    designing new post page

--- app/blog/new/NewBlogForm.tsx
+++ app/blog/new/NewBlogForm.tsx
@@ -31,20 +31,32 @@ const NewBlogForm = (props: Props) => {
   if (submitted) return <div>Post Submitted!</div>;
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="min-h-[calc(100vh-130px)] py-2 container flex flex-col mt-12">
+      <form
+        className="flex flex-col flex-1 items-stretch justify-center h-full text-left"
+        onSubmit={handleSubmit}
+      >
         <input
+          className="text-6xl focus-visible:outline-none shadow-[0px_0.25rem_0px_0px_rgba(0,0,0,1)]"
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
           name="title"
         />
         <textarea
+          className="flex-1 focus-visible:outline-none text-4xl mt-8"
           value={content}
+          placeholder="Content:"
           onChange={(e) => setContent(e.target.value)}
           name="content"
         />
-        <button type="submit">Submit</button>
+        <button
+          className="w-fit-content text-white bg-indigo-400 px-4 py-2 sm:px-6 sm:py-4 mt-6 border-2 rounded shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)]"
+          type="submit"
+        >
+          Create
+        </button>
       </form>
     </div>
   );
